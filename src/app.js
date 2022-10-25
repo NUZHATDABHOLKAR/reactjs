9c0578bd6ecaf31f8a15eef1579bf5df3381607a
NUZHAT KASAM  DABHOLKAR <93029178+NUZHATDABHOLKAR@users.noreply.github.com>
2022-10-25 15:46:35 +0530

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import Happy from './happy'
+function  App(){
+
+    return(
+        <>
+        <h2>today is your day -nuzu</h2>
+        <p>
+            its very important to find your own solution .sometimes its very difficult to know yourself .spend time with yourself that is neccessay learn new thing it increase your thinking power.
+        </p>
+        <Happy/>
+       
+        <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.goodmorningquote.com%2Fgood-morning-quotes-images%2F&psig=AOvVaw1_DfPQuHnme-TRfBb3Xog2&ust=1666248835369000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCOjUleXa6_oCFQAAAAAdAAAAABA0" alt=".." /> }
+        </>
+    )
+}
+export default App
---
+++ src/happy.js
@@ -0,0 +1,8 @@
+function Happy(){
+    return(
+       <>
+         <a href="https://goodmorningland.com/wp-content/uploads/2020/04/Choose-the-best-Good-Morning-Quotes-Images-from-our-stock.jpg">YOU WAKE UP </a>
+       </> 
+    )
+}
+export default Happy
